55077b053224e5db00fbb5f70b4c3078f8394cc7
kotathefriends <5070kouta@gmail.com>
2024-03-14 15:43:59 +0900

    Initial Commit

---
+++ CarouselView/CarouselViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselViewApp.swift
+//  CarouselView
+//
+//  Created by KOTA TAKAHASHI on 2024/03/14.
+//
+
+import SwiftUI
+
+@main
+struct CarouselViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
